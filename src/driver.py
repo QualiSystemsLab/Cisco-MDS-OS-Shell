57746cd359a893389d38e8213d73dbb234f64b9b
greg-s <gregory.s@qualisystems.com>
2016-12-14 10:53:44 -0800

    Initial commit

---
+++ src/driver.py
@@ -0,0 +1,114 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.context import InitCommandContext, ResourceCommandContext
+from cloudshell.api.cloudshell_api import CloudShellAPISession
+from QualiLab_CLI import Cli_Lib
+
+
+class MdsDriver (ResourceDriverInterface):
+
+    def cleanup(self):
+        """
+        Destroy the driver session, this function is called everytime a driver instance is destroyed
+        This is a good place to close any open sessions, finish writing to log files
+        """
+        pass
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def _dumblog(self, message):
+        with open('c:\\temp\\dumblog.txt', 'a') as log:
+            log.write(message + '\n')
+
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called everytime a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        pass
+
+
+    def _get_api_session(self, context):
+
+        """
+
+        :param ResourceCommandContext context:
+        :return:
+        """
+        return CloudShellAPISession(context.connectivity.server_address, token_id=context.connectivity.admin_auth_token,
+                                    domain=context.reservation.domain)
+
+    def _decrypt_password(self, context, password):
+        """
+
+        :param ResourceCommandContext context:
+        :return:
+        """
+        api = self._get_api_session(context)
+
+        return api.DecryptPassword(password).Value
+
+    def _get_cli_session(self, context):
+        """
+
+        :param ResourceCommandContext context:
+        :return:
+        """
+
+        cli_session = Cli_Lib.Cli(context.resource.address, int(context.resource.attributes['Console Port']), 'SSH',
+                                  context.resource.attributes['User'],
+                                  self._decrypt_password(context, context.resource.attributes['Password']))
+
+        self._dumblog('got session object')
+        cli_session.login()
+        self._dumblog('logged in')
+
+
+        return cli_session
+
+
+    def create_zone(self, context, zone_name):
+
+        cli = self._get_cli_session(context)
+
+        cli.send_and_receive('config t')
+        cli.send_and_receive('zone name ' + zone_name + ' vsan ' + context.resource.attributes['vsan'])
+
+    def get_active_zoneset_name(self, context):
+        pass
+        #todo
+
+    def add_zone_to_zoneset(self, context, zone_name):
+        cli = self._get_cli_session(context)
+        cli.send_and_receive('config t')
+        cli.send_and_receive('zoneset name ' + context.resource.attributes['zoneset'] + ' vsan ' +
+                             context.resource.attributes['vsan'])
+        cli.send_and_receive('member ' + zone_name)
+
+
+    def add_wwn_to_zone(self, context, zone_name, wwn):
+
+        cli = self._get_cli_session(context)
+        cli.send_and_receive('config t')
+        cli.send_and_receive('zone name ' + zone_name + ' vsan ' + context.resource.attributes['vsan'])
+        cli.send_and_receive('member pwwn ' + wwn)
+
+
+    def activate_zoneset(self, context):
+
+        cli = self._get_cli_session(context)
+
+        cli.send_and_receive('config t')
+        cli.send_and_receive('zoneset activate name ' + context.resource.attributes['zoneset'] + ' vsan ' +
+                             context.resource.attributes['vsan'])
+
+
+    def delete_zone(self, context, zone_name):
+        cli = self._get_cli_session(context)
+        cli.send_and_receive('config t')
+        cli.send_and_receive('no zone name ' + zone_name + ' vsan ' + context.resource.attributes['vsan'])
